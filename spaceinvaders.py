e1fd5d36c2bddbd49862602f1ba596762db1f7f9
cthulhu2 <qwe@qwe.qwe>
2018-10-02 05:25:40 +0200

    Refactor mystery conditions w velocity

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -219,44 +219,31 @@ class Blocker(Sprite):
 class Mystery(Sprite):
     def __init__(self, *groups):
         Sprite.__init__(self, *groups)
-        self.image = IMAGES['mystery']
-        self.image = transform.scale(self.image, (75, 35))
+        self.image = transform.scale(IMAGES['mystery'], (75, 35))
         self.rect = self.image.get_rect(topleft=(-80, 45))
         self.row = 5
         self.moveTime = 25000
-        self.direction = 1
+        self.velocity = 2
         self.timer = time.get_ticks()
-        self.mysteryEntered = mixer.Sound(SOUND_PATH + 'mysteryentered.wav')
+        self.mysteryEntered = Sound(SOUND_PATH + 'mysteryentered.wav')
         self.mysteryEntered.set_volume(0.3)
         self.playSound = True
 
     # noinspection PyUnusedLocal
     def update(self, keys, current_time, *args):
-        reset_timer = False
         passed = current_time - self.timer
         if passed > self.moveTime:
             if (self.rect.x < 0 or self.rect.x > 800) and self.playSound:
                 self.mysteryEntered.play()
                 self.playSound = False
-            if self.rect.x < 840 and self.direction == 1:
+            if -100 < self.rect.x < 840:
                 self.mysteryEntered.fadeout(4000)
-                self.rect.x += 2
+                self.rect.x += self.velocity
                 game.screen.blit(self.image, self.rect)
-            elif self.rect.x > -100 and self.direction == -1:
-                self.mysteryEntered.fadeout(4000)
-                self.rect.x -= 2
-                game.screen.blit(self.image, self.rect)
-
-        if self.rect.x > 830:
-            self.playSound = True
-            self.direction = -1
-            reset_timer = True
-        elif self.rect.x < -90:
-            self.playSound = True
-            self.direction = 1
-            reset_timer = True
-        if passed > self.moveTime and reset_timer:
-            self.timer = current_time
+            if self.rect.x < -90 or self.rect.x > 830:
+                self.playSound = True
+                self.velocity *= -1
+                self.timer = current_time
 
 
 class EnemyExplosion(Sprite):
