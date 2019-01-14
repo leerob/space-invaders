2f3e44af242abea9e80dcd4e02e5d7bf6b986cc4
cthulhu2 <qwe@qwe.qwe>
2019-01-14 15:30:05 +0200

    Refactor background drawing

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -539,16 +539,9 @@ class SpaceInvaders(object):
             self.shipAlive = True
 
     def create_game_over(self, currentTime):
-        self.screen.blit(self.background, (0, 0))
         passed = currentTime - self.timer
-        if passed < 750:
+        if passed < 750 or 1500 < passed < 2250:
             self.gameOverText.draw(self.screen)
-        elif 750 < passed < 1500:
-            self.screen.blit(self.background, (0, 0))
-        elif 1500 < passed < 2250:
-            self.gameOverText.draw(self.screen)
-        elif 2250 < passed < 2750:
-            self.screen.blit(self.background, (0, 0))
         elif passed > 3000:
             self.mainScreen = True
 
@@ -558,8 +551,8 @@ class SpaceInvaders(object):
 
     def main(self):
         while True:
+            self.screen.blit(self.background, (0, 0))
             if self.mainScreen:
-                self.screen.blit(self.background, (0, 0))
                 self.titleText.draw(self.screen)
                 self.titleText2.draw(self.screen)
                 self.enemy1Text.draw(self.screen)
@@ -585,7 +578,6 @@ class SpaceInvaders(object):
                 if not self.enemies and not self.explosionsGroup:
                     currentTime = time.get_ticks()
                     if currentTime - self.gameTimer < 3000:
-                        self.screen.blit(self.background, (0, 0))
                         self.scoreText2 = Text(FONT, 20, str(self.score),
                                                GREEN, 85, 5)
                         self.scoreText.draw(self.screen)
@@ -602,7 +594,6 @@ class SpaceInvaders(object):
                 else:
                     currentTime = time.get_ticks()
                     self.play_main_music(currentTime)
-                    self.screen.blit(self.background, (0, 0))
                     self.allBlockers.update(self.screen)
                     self.scoreText2 = Text(FONT, 20, str(self.score), GREEN,
                                            85, 5)
